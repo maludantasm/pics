37b5e4207969986541777ca4e0ba0026b831c16b
maludantasm <maludantas11@gmail.com>
2020-07-14 11:27:09 -0300

    +pics

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 'use strict';
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+function showDate() {
+    let today = new Date();
+    let formatDate = today.toDateString();
+    let selectElement = document.getElementById('date');
+    selectElement.innerHTML = formatDate;
+    console.log ('here\'s a hidden message');
+}
+
+function msgMalu(txtMalu) {
+    alert(txtMalu);
+}
+
+function msgMalu2(txtMalu) {
+    document.body.innerHTML += '<br>' + txtMalu;
+}
